560f91b6d2678fda80fd9887949b3a4de453c0ea
aashirwad89 <studyonly806@gmail.com>
2025-11-17 17:19:52 +0530

    Strings done

--- Strings.java
+++ Strings.java
@@ -1,3 +1,4 @@
+import java.sql.Struct;
 import java.util.*;
 public class Strings {
     // public static void printLetters(String str){
@@ -16,26 +17,84 @@ public class Strings {
 //     }
 // }
 
-public static float shortPath(String str){
-    int x =0, y=0;
+// public static float shortPath(String str){
+//     int x =0, y=0;
 
-    for(int i =0; i<str.length(); i++){
-        char dir = str.charAt(i);
-        // south
-        if(dir == "S"){
-            y--;
-        }else if(dir == "N"){ //north
-y++;
-        }else if(dir == "W"){//west
-            x--;
-        }else{ //east
-            x++;
-        }
+//     for(int i =0; i<str.length(); i++){
+//         char dir = str.charAt(i);
+//         // south
+//         if(dir == 'S'){
+//             y--;
+//         }else if(dir == 'N'){ //north
+// y++;
+//         }else if(dir == 'W'){//west
+//             x--;
+//         }else{ //east
+//             x++;
+//         }
+//     }
+//     int X2 = x*x;
+//     int Y2 = y*y;
+//     return  (float)Math.sqrt(X2 + Y2);
+// };
+
+
+// public static String printSub(String str, int si , int ei){
+// String substr = "";
+// for(int i=si; i<ei; i++){
+//     substr += str.charAt(i);
+// }
+// return substr;
+// }
+
+// public static String toUpperCase(String str){
+//     StringBuilder sb = new StringBuilder("");
+
+//     char ch = Character.toUpperCase(str.charAt(0));
+//     sb.append(ch);
+
+//     for(int i=1; i<str.length(); i++){
+//         if(str.charAt(i) == ' ' && i<str.length()-1) {
+//             sb.append(str.charAt(i));
+//             i++;
+// sb.append(Character.toUpperCase(str.charAt(i))); 
+//         }else{
+//             sb.append(str.charAt(i));
+//         }
+//     }
+//     return sb.toString();
+// }
+
+// public static String compress(String str){
+//     String newStr  = " ";
+//     for(int i=0; i<str.length(); i++){
+//         Integer count =1;
+//         while(i<str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+//             count++;
+//             i++;
+
+//         }
+//         newStr += str.charAt(i);
+//         if(count > 1){
+//             newStr += count.toString();
+        
+//         }
+//     }
+//     return newStr;
+
+
+// }
+
+public static String toVowels(String str){
+    for(int i=0; i<str.length(); i++){
+        
     }
-    int X2 = x*x;
-    int Y2 = y*y;
-    return  (float)Math.sqrt(X2 + Y2);
-};
+}
+
+
+
+
+
     public static void main(String[] args) {
         // char arr[] = {'a', 'b', 'c', 'd'};
         // String str = "abcd";
@@ -58,9 +117,44 @@ y++;
     // String str = "racecar";
     // System.out.println(palinPrint(str));
 
-    String str = "WNEENESENNN";
-    System.out.println(shortPath(str));
+    // String str = "WNEENESENNN";
+    // System.out.println(shortPath(str));
+
+    // String str = "HelloWorld";
+    
+    // System.out.println(str.substring(0,5));
+    // System.out.println(printSub(str, 0, 5));
+
+
+//     String fruits[] = {"apple", "mango", "banana"};
+// String largest = fruits[0];
+// for(int i=1; i<fruits.length;i++){
+//      if(largest.compareToIgnoreCase(fruits[i]) < 0){
+// largest = fruits[i];
+//      }
+// }
+
+// System.out.println(largest);
+
+// StringBuilder sb = new StringBuilder("");
+// for(char ch ='a'; ch<='z'; ch++){
+//     sb.append(ch);
+// }
+// System.out.println(sb);
+
+// String str = "hi , i am shraddha";
+// System.out.println(toUpperCase(str));
+
+// String str = "aaabbcccdd";
+// System.out.println(compress(str));
+
+Scanner sc = new Scanner(System.in);
+String str = sc.nextLine();
+
+
+
 
     }
+
+
 }
- 
