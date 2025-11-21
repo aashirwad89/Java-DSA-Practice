61b4f354750bb0598bd33bb9b961397888aa3512
aashirwad89 <studyonly806@gmail.com>
2025-11-21 18:39:06 +0530

    Practice 1 problem

--- Practice.java
+++ Practice.java
@@ -1,3 +1,16 @@
 public class Practice {
+
+    public static boolean isPalindrome(String s){
+        for(int i = 0; i<s.length()/2; i++){
+            int n = s.length();
+            if(s.charAt(i) != s.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+      String s = "abba";
+      System.out.println(isPalindrome(s));  
+    }}
     
-}
