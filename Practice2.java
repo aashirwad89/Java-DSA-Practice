c67c65e23bfe20fd4808e7a5bf482125d5c0ca9f
aashirwad89 <studyonly806@gmail.com>
2025-12-22 16:40:07 +0530

    divide and conquer

--- Practice2.java
+++ Practice2.java
@@ -1,108 +1,122 @@
-public class Practice2 {
+// public class Practice2 {
 
-public static boolean isEven(int n){
-    return n % 2 == 0;
-}
-
-public static int numMax(int a , int b){
-    if(a<b){
-        return b;
-    }else{
-        return a;
-    }
-}
-
-public static int printFact(int n , int fact){
-    for(int i=1; i<=n; i++ ){
-fact *= i;
-    }
-    return fact;
-}
-
-
-
-    public static void main(String[] args) {
-        // int sum = 0;
-        // int n = 5;
-        // for(int i =0; i<=n; i++){
-        //     sum += i;
-        // }
-        // System.out.println(sum);
-
-        // int n = 5;
-        // int fact = 1;
-        // for(int i=1; i<=n; i++){
-        //     fact *= i;
-        // }
-        // System.out.println(fact);
-
-
-        // int n = 12345;
-        // int count = 0;
-        // while(n!=0){
-        //     count++;
-        //     n /= 10;
-        // }
-        // System.out.println(count);
-// System.out.println(isEven(10));
-
-// System.out.println(numMax(5, 2));
-
-// System.out.println(printFact(5, 1));
-
-// int arr[] = {2,3,4,5,6};
-// int sum = 0;
-
-// for(int i=0; i<arr.length; i++){
-//     sum += arr[i];
+// public static boolean isEven(int n){
+//     return n % 2 == 0;
 // }
-// System.out.println(sum);
 
-// int arr[] = {2,3,4,5,6};
-// int max = arr[0];
-
-// for(int i =0; i<arr.length; i++){
-//     if(arr[i] > max){
-//         max = arr[i];
+// public static int numMax(int a , int b){
+//     if(a<b){
+//         return b;
+//     }else{
+//         return a;
 //     }
 // }
-// System.out.println(max);
 
-// int arr[] = {2,3,4,5,6};
-
-// for(int i= arr.length-1; i>=0; i--){
-//     System.out.println(arr[i]);
-// }
-
-// String str = "education";
-// int count = 0;
-// for(int i=0; i<str.length(); i++){
-//     char ch = str.charAt(i);
-//     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-//         count++;
+// public static int printFact(int n , int fact){
+//     for(int i=1; i<=n; i++ ){
+// fact *= i;
 //     }
-//     System.out.println(count);
+//     return fact;
 // }
 
-// String str = "drop";
-// String rev = "";
-// for(int i = str.length()-1 ; i>=0; i--){
-//     rev += str.charAt(i);
+// public static void printArr(int arr[]){
+//     for(int i=0; i<arr.length; i++){
+//         System.out.println(arr[i]);
+//     }
+//     System.out.println();
 // }
-// System.out.println(rev);
 
-
-// String str = "madam";
-// String rev = "";
-// for(int i = str.length()-1; i>=0; i--){
-//     rev += str.charAt(i);
-// }
-// if(str.equals(rev)){
-//     System.out.println("Palindrom");
-// }else{
-//     System.out.println("Not a palindrom");
+// public static void mergeSort(int arr[] , int si , int ei){
+// // work
+// int mid = si + (si-ei)/2 ;
+// mergeSort(arr, si, mid); //left part
+// mergeSort(arr, mid+1, ei); //right part
 // }
 
 
-    }
-}
+//     public static void main(String[] args) {
+//         // int sum = 0;
+//         // int n = 5;
+//         // for(int i =0; i<=n; i++){
+//         //     sum += i;
+//         // }
+//         // System.out.println(sum);
+
+//         // int n = 5;
+//         // int fact = 1;
+//         // for(int i=1; i<=n; i++){
+//         //     fact *= i;
+//         // }
+//         // System.out.println(fact);
+
+
+//         // int n = 12345;
+//         // int count = 0;
+//         // while(n!=0){
+//         //     count++;
+//         //     n /= 10;
+//         // }
+//         // System.out.println(count);
+// // System.out.println(isEven(10));
+
+// // System.out.println(numMax(5, 2));
+
+// // System.out.println(printFact(5, 1));
+
+// // int arr[] = {2,3,4,5,6};
+// // int sum = 0;
+
+// // for(int i=0; i<arr.length; i++){
+// //     sum += arr[i];
+// // }
+// // System.out.println(sum);
+
+// // int arr[] = {2,3,4,5,6};
+// // int max = arr[0];
+
+// // for(int i =0; i<arr.length; i++){
+// //     if(arr[i] > max){
+// //         max = arr[i];
+// //     }
+// // }
+// // System.out.println(max);
+
+// // int arr[] = {2,3,4,5,6};
+
+// // for(int i= arr.length-1; i>=0; i--){
+// //     System.out.println(arr[i]);
+// // }
+
+// // String str = "education";
+// // int count = 0;
+// // for(int i=0; i<str.length(); i++){
+// //     char ch = str.charAt(i);
+// //     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+// //         count++;
+// //     }
+// //     System.out.println(count);
+// // }
+
+// // String str = "drop";
+// // String rev = "";
+// // for(int i = str.length()-1 ; i>=0; i--){
+// //     rev += str.charAt(i);
+// // }
+// // System.out.println(rev);
+
+
+// // String str = "madam";
+// // String rev = "";
+// // for(int i = str.length()-1; i>=0; i--){
+// //     rev += str.charAt(i);
+// // }
+// // if(str.equals(rev)){
+// //     System.out.println("Palindrom");
+// // }else{
+// //     System.out.println("Not a palindrom");
+// // }
+// int arr[] = {6,3,9,5,2,8};
+// mergeSort(arr, 0, arr.length-1);
+
+//     }
+// }
