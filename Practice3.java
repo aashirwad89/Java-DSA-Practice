f004fdcba31df6e5e9bbcc4b09218939ce5a5389
aashirwad89 <studyonly806@gmail.com>
2026-01-02 10:44:03 +0530

    Practice3

--- Practice3.java
+++ Practice3.java
@@ -1,9 +1,13 @@
-
+import java.util.*;
 public class Practice3{
+
+// public static String toLower(String s){
+//     String lower = s.toLowerCase();
+// }
 
     
     public static void main(String[] args) {
         int arr[] = {"sun", "earth", "mars", "mercury"};
-
+// String s = "ABCD";        
     }
 }
