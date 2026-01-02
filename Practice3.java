5c52b8e69f9f3c40799c1336e2a5397a58a9c3fd
aashirwad89 <studyonly806@gmail.com>
2026-01-03 01:34:58 +0530

    aa

--- Practice3.java
+++ Practice3.java
@@ -5,9 +5,49 @@ public class Practice3{
 //     String lower = s.toLowerCase();
 // }
 
-    
+public static void mergeSort(String arr[], int si , int ei){
+    if(si>=ei){
+        return;
+    }
+    int mid = si+(ei-si)/2;
+
+    mergeSort(arr, si, mid);
+    mergeSort(arr, mid+1, ei);
+
+    merge(arr, si, mid, ei);
+}    
+public static void merge(String arr[], int si , int mid , int ei){
+    String[] temp = new String[ei-si+1];
+    int i = si;
+    int j = mid+1;
+    int k = 0;
+
+
+    while(i<=mid && j<=ei){
+        if(arr[i].compareTo(arr[j])<=0){
+            temp[k++] = arr[i++];
+        }else{
+            temp[k++] = arr[j++];
+        }
+    }
+
+    while(i<=mid){
+        temp[k++] = arr[i++];
+    }
+
+    while(j<=ei){
+        temp[k++] = arr[j++];
+    }
+
+    for(int x =0; x<temp.length ; x++){
+        arr[si+x] = temp[x];
+    }
+}
+
     public static void main(String[] args) {
-        int arr[] = {"sun", "earth", "mars", "mercury"};
-// String s = "ABCD";        
+        String arr[] = {"sun", "earth", "mars", "mercury"};
+// String s = "ABCD";  
+System.out.println(mergeSort(arr, 0, arr.length-1));
+    
     }
 }
