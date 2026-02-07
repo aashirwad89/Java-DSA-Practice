0b4845d0d627f9246e580e8ec02c7d3a4801050b
aashirwad89 <studyonly806@gmail.com>
2026-02-07 10:21:31 +0530

    Arrays revison done

--- Practice3.java
+++ Practice3.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 public class Practice3{
 
@@ -81,6 +82,95 @@ public static void reverseArr(int numbers[]){
         last--;
     }
 }
+
+public static void bubbleSort(int arr[]){
+
+    int n = arr.length;
+for(int i=0; i<n-1; i++){
+    for(int j=0; j<n-1-i; j++){
+        if(arr[j]>arr[j+1]){
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+}
+}
+
+public static void selectionSort(int arr[]){
+    int n = arr.length;
+    for(int i=0; i<n-1; i++){
+int minIdx = i;
+
+for(int j=i+1; j<n; j++){
+    if(arr[j]<arr[minIdx]){
+        minIdx = j;
+    }
+}
+// swap 
+int temp = arr[minIdx];
+arr[minIdx] = arr[i];
+arr[i] = temp;
+    }
+}
+
+public static void insertionSort(int arr[]){
+    int n = arr.length;
+for(int i=0; i<n; i++){
+    int key = arr[i];
+    int j = i-1;
+
+    while(j>=0 && arr[j]>key){
+        arr[j+1] = arr[j];
+        j--;
+    }
+
+    arr[j+1] = key;
+}
+
+}
+
+public static void printSpiral(int matrix[][]) {
+
+    int startRow = 0;
+    int startCol = 0;
+    int endRow = matrix.length - 1;
+    int endCol = matrix[0].length - 1;
+
+    while (startRow <= endRow && startCol <= endCol) {
+
+        // top
+        for (int j = startCol; j <= endCol; j++) {
+            System.out.print(matrix[startRow][j] + " ");
+        }
+
+        // right
+        for (int i = startRow + 1; i <= endRow; i++) {
+            System.out.print(matrix[i][endCol] + " ");
+        }
+
+        // bottom
+        if (startRow < endRow) {
+            for (int j = endCol - 1; j >= startCol; j--) {
+                System.out.print(matrix[endRow][j] + " ");
+            }
+        }
+
+        // left
+        if (startCol < endCol) {
+            for (int i = endRow - 1; i > startRow; i--) {
+                System.out.print(matrix[i][startCol] + " ");
+            }
+        }
+
+        startRow++;
+        startCol++;
+        endRow--;
+        endCol--;
+    }
+}
+
+
     public static void main(String[] args) {
 //         String arr[] = {"sun", "earth", "mars", "mercury"};
 // // String s = "ABCD";  
@@ -106,14 +196,45 @@ public static void reverseArr(int numbers[]){
 
 // System.out.println(Bsearch(num, key));
 
-// reversing an array 
-int numbers[] = {3,4,53,2};
-reverseArr(numbers);
-for(int i: numbers){
-    System.out.println(i);
-}
+// // reversing an array 
+// int numbers[] = {3,4,53,2};
+// reverseArr(numbers);
+// for(int i: numbers){
+//     System.out.println(i);
+// }
 
+// int arr[] = {5,4,1,3,2};
+// bubbleSort(arr);
+// for(int num:arr){
+//     System.out.println(num);
+// }
 
-    
+// selection sort 
+// int arr[] = {5,4,2,3,1};
+
+// selectionSort(arr);
+// for(int num : arr){
+//     System.out.println(num);
+// }
+
+// insertion sort 
+// int arr[] = {5,4,2,3,1};
+
+// insertionSort(arr);
+// for(int num:arr){
+//     System.out.println(num);
+// }
+
+// // inbuilt sort 
+// int arr[] = {5,4,2,1,3};
+// Arrays.sort(arr);
+// System.out.println(arr);
+
+int matrix[][] = {
+    {1,2,3,4},
+    {5,6,7,8},
+    {9,10,11,12}
+};
+    printSpiral(matrix);
     }
 }
