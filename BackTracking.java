c3f361be10f72edc7c36f9ae868e57ef7efb4e44
aashirwad89 <studyonly806@gmail.com>
2026-02-11 16:03:07 +0530

    just

--- BackTracking.java
+++ BackTracking.java
@@ -52,6 +52,28 @@ for(int i=0; i<str.length(); i++){
 }
 }
 
+public static int nQueens(char board[][], int row){
+// base case 
+if(row == board.length){
+   printBoard(board);
+    return 1; 
+}
+    // col
+    for(int j =0; j<board.length; j++){
+        board[row][j] = 'Q';
+        nQueens(board, row+1);
+        board[row][j] = '.';
+    }
+}
+ public static void printBoard(char board[][]){
+    for(int i = 0; i<board.length; i++){
+        for(int j = 0 ; j<board.length; j++){
+            System.out.print(board[i][j] + " ");
+        }
+        System.out.println();
+    }
+ }
+
     public static void main(String[] args) {
         // int arr[] = new int[5];
         // changeArr(arr, 0, 1);
@@ -61,7 +83,18 @@ for(int i=0; i<str.length(); i++){
         // String str  = "abc";
         // findSubset(str, "", 0);
 
-        String str = "abc";
-findPermutation(str, "");
+//         String str = "abc";
+// findPermutation(str, "");
+
+int n = 2;
+char board[][] = new char[n][n];
+
+// intialize
+for(int i=0; i<n; i++){
+    for(int j =0; j<n; j++){
+        board[i][j] = '.';
+    }
+}
+nQueens(board, 0);
     }
 }
