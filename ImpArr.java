cb3679ab425d17466c33f7832aaaa1d506257391
aashirwad89 <studyonly806@gmail.com>
2026-02-13 10:59:07 +0530

    Trapped rainwater

--- ImpArr.java
+++ ImpArr.java
@@ -64,10 +64,43 @@ for(int i =0; i<numbers.length; i++){
 }
 System.out.println(maxSum);
 }
+
+public static int trappedWater(int height[]){
+    // calculate left max boundary - array
+    int leftMax[] = new int[height.length];
+    leftMax[0] = height[0];
+    for(int i = 1; i<height.length; i++){
+        leftMax[i] = Math.max(height[i], leftMax[i-1]);
+    }
+    
+
+// calculate right max boundary - array
+int rightMax[] = new int[height.length];
+rightMax[height.length-1] = height[height.length-1];
+for(int i = height.length-2; i>=0; i--){
+rightMax[i] = Math.max(height[i], rightMax[i+1]);
+}
+
+
+int trappedWater = 0;
+// loop
+for(int i=0; i<height.length; i++){
+ //water level = min(leftMax bound , rightMax bound)
+ int waterLevel = Math.min(leftMax[i], rightMax[i]);
+ // trapped water = waterLevel - height 
+trappedWater += waterLevel -height[i];
+}
+
+return trappedWater;
+}
     public static void main(String[] args) {
-        int numbers[] = {1,2,3,-2,6};
+        // int numbers[] = {1,2,3,-2,6};
         // printSubArr(numbers);
         // printPrefixArr(numbers);
-        kandanes(numbers);
+        // kandanes(numbers);
+
+        // trapped Rainwater
+        int height[] = {4,2,0,6,3,2,5};
+System.out.println(trappedWater(height));
     }
 }
