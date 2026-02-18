a04dda074c3c7ca18caaf0e4b71505c9f76dddc9
aashirwad89 <studyonly806@gmail.com>
2026-02-18 14:58:07 +0530

    Arrays list finish

--- ArraysList.java
+++ ArraysList.java
@@ -9,20 +9,101 @@ public class ArraysList {
     }
 
     // this is a bruteforce approach for most water
-    public static int storeWater(ArrayList<Integer> height){
-int maxWater = 0 ;
-for(int i=0; i<height.size(); i++){
-    for(int j = i+1; j<height.size(); j++){
-        int ht = Math.min(height.get(i), height.get(j));
-        int wd = j-i;
-        int currWater = ht*wd;
-        maxWater  = Math.max(maxWater, currWater);
+//     public static int storeWater(ArrayList<Integer> height){
+// int maxWater = 0 ;
+// for(int i=0; i<height.size(); i++){
+//     for(int j = i+1; j<height.size(); j++){
+//         int ht = Math.min(height.get(i), height.get(j));
+//         int wd = j-i;
+//         int currWater = ht*wd;
+//         maxWater  = Math.max(maxWater, currWater);
+//     }
+// }
+
+// return maxWater;
+//     }
+// 
+
+// 2 pointer approach for most water contained
+public static int storeWater(ArrayList<Integer> height){
+int maxWater = 0;
+int lp = 0;
+int rp = height.size()-1;
+
+while (lp<rp) {
+    // calculate water area
+    int hgt = Math.min(height.get(lp), height.get(rp));
+    int wdt = rp-lp;
+    int currWater = hgt * wdt;
+    maxWater = Math.max(maxWater, currWater);
+
+    // update ptr
+    if(height.get(lp)< height.get(rp)){
+        lp++;
+    }else{
+        rp--;
     }
 }
-
 return maxWater;
+}
+
+// brute force approach
+// public static boolean pairSum(ArrayList<Integer> height, int target){
+
+//     for(int i =0; i<=height.size(); i++){
+//         for(int j = i+1; j<height.size(); j++){
+//             if(height.get(i) + height.get(j) == target){
+// return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+    // 2pointer approach - linear time complexity dega 
+//     public static boolean pairSum(ArrayList<Integer> height, int target){
+// int lp = 0;
+// int rp = height.size()-1;
+
+// while(lp != rp){
+//     if(height.get(lp) + height.get(rp) == target){
+//         return true;
+//     }
+
+//     if(height.get(lp)+height.get(rp) < target){
+//         lp++;
+//     }else{
+//         rp--;
+//     }
+// }
+// return false;
+//     }
+
+public static boolean pairSum(ArrayList<Integer> list , int target){
+    int bp = -1;
+    int n = list.size();
+    for(int i =0; i<list.size(); i++){
+        if(list.get(i) > list.get(i+1)){
+            bp = i;
+            break;
+        }
+    }
+    int lp = bp+1; //smallest
+    int rp = bp; //largest 
+
+    while(lp != rp){
+        if(list.get(lp) + list.get(rp) == target){
+            return true;
+        }
+         if(list.get(lp) + list.get(rp) < target){
+            lp = (lp+1) % n;
+         }else{
+            rp = (n+rp-1) % n;
+         }
     }
 
+    return false;
+}
     public static void main(String[] args) {
         // ArrayList<Integer> list = new ArrayList<>(); 
 
@@ -104,17 +185,27 @@ return maxWater;
 
 
 // container with most water - with brute force approach 
-ArrayList<Integer> height = new ArrayList<>();
-height.add(1);
-height.add(8);
-height.add(6);
-height.add(2);
-height.add(5);
-height.add(4);
-height.add(8);
-height.add(3);
-height.add(7);
+ArrayList<Integer> list = new ArrayList<>();
+list.add(11);
+list.add(15);
+list.add(6);
+list.add(8);
+list.add(9);
+list.add(10);
 
-System.out.println(storeWater(height));
+int target = 16;
+
+System.out.println(pairSum(list, target));
+// height.add(4);
+// height.add(8);
+// height.add(3);
+// height.add(7);
+
+// System.out.println(storeWater(height));
+
+// pair sum -1 
+
+
+
     }
 }
