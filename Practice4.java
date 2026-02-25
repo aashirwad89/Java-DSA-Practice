2f3884eebc52724a2232d098b88b9dd869fdf25e
aashirwad89 <studyonly806@gmail.com>
2026-02-25 14:42:44 +0530

    star pattern solve

--- Practice4.java
+++ Practice4.java
@@ -1,26 +1,13 @@
 public class Practice4 {
-    public static int singleNumber(int[] nums) {
-        int result = 0;
-
-        for (int i = 0; i < 32; i++) {
-            int sum = 0;
-
-            for (int num : nums) {
-                if ((num >> i & 1) == 1) {
-                    sum++;
-                }
-            }
-
-            if (sum % 3 != 0) {
-                result = result | (1 << i);
-            }
-        }
-
-        return result;
-    }
+   
 
     public static void main(String[] args) {
-        int[] nums = {2,2,2,4,4,4,9};
-        System.out.println(singleNumber(nums));  // 9
+        int n = 4;
+        for(int line = 1; line <= n; line++){
+            for(int stars = 1; stars <= n-line+1; stars++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
