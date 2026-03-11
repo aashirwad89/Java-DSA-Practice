34c220dd7ad27cfbf0e7f124b84950856e2ab9fd
aashirwad89 <studyonly806@gmail.com>
2026-03-11 16:02:19 +0530

    reverse stack

--- StackB.java
+++ StackB.java
@@ -29,6 +29,24 @@ public static String reverseString(String str){
 
    return  str = result.toString();
 }
+
+public static void reverStack(Stack<Integer> s){
+    if(!s.isEmpty()){
+        return ;
+    }
+
+    int top = s.pop();
+    reverStack(s);
+
+    pushAtBottom(s, top);
+
+}
+
+public static void printStack(Stack<Integer> s){
+    while(!s.isEmpty()){
+        System.out.println(s.pop());
+    }
+}
     public static void main(String[] args) {
         // Stack<Integer> s = new Stack<>();
         // s.push(1);
@@ -40,8 +58,17 @@ public static String reverseString(String str){
         //     System.out.println(s.pop());
         // }
 
-        String str = "abc";
-       String result =  reverseString(str);
-System.out.println(result);
+//         String str = "abc";
+//        String result =  reverseString(str);
+// System.out.println(result);
+
+Stack<Integer> s = new Stack<>();
+s.push(1);
+s.push(2);
+s.push(3);
+
+reverStack(s);
+
+printStack(s);
     }
 }
