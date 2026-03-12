bbdb5ec64835193c57b3d1f6a4962362a2558b8c
aashirwad89 <studyonly806@gmail.com>
2026-03-12 13:11:23 +0530

    stack

--- StackB.java
+++ StackB.java
@@ -47,6 +47,26 @@ public static void printStack(Stack<Integer> s){
         System.out.println(s.pop());
     }
 }
+
+public static void stockSpan(int stocks[], int span[]){
+    Stack<Integer> s = new Stack<>();
+
+    span[0] = 1; 
+    s.push(0);
+    for(int i = 1; i<stocks.length; i++){
+        int currPrice = stocks[i];
+        while(!s.isEmpty() && currPrice > stocks[s.peek()]){
+            s.pop();
+        }
+        if(s.isEmpty()){
+            span[i] = i+1;
+        }else{
+            int prevHigh = s.peek();
+            span[i] = i-prevHigh;
+        }
+        s.push(i);
+    }
+}
     public static void main(String[] args) {
         // Stack<Integer> s = new Stack<>();
         // s.push(1);
@@ -62,13 +82,22 @@ public static void printStack(Stack<Integer> s){
 //        String result =  reverseString(str);
 // System.out.println(result);
 
-Stack<Integer> s = new Stack<>();
-s.push(1);
-s.push(2);
-s.push(3);
+// Stack<Integer> s = new Stack<>();
+// s.push(1);
+// s.push(2);
+// s.push(3);
 
-reverStack(s);
+// reverStack(s);
 
-printStack(s);
+// printStack(s);
+
+
+int stocks[] = {100,85, 60 , 70, 60, 85, 100};
+int span[] = new int[stocks.length];
+stockSpan(stocks, span);
+
+for(int i = 0; i<span.length; i++){
+    System.out.println(span[i] + " ");
+}
     }
 }
