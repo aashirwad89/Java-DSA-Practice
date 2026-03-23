9a2ab81375238915600c3a8b7b54ea4643e89eec
aashirwad89 <studyonly806@gmail.com>
2026-03-23 15:54:39 +0530

    push

--- Queue.java
+++ Queue.java
@@ -1,71 +1,83 @@
+import java.util.Stack;
+import java.utils.*;
+
 public class Queue {
-    static class Node{
-        int data;
-         Node next;
+//     static class Node{
+//         int data;
+//          Node next;
 
-         Node(int data){
-            this.data = data;
-            this.next = null;
-         }
-    }
-    static class QueueB{
-static Node head = null;
-static Node tail = null;
+//          Node(int data){
+//             this.data = data;
+//             this.next = null;
+//          }
+//     }
+//     static class QueueB{
+// static Node head = null;
+// static Node tail = null;
 
 
 
-public static boolean isEmpty(){
-  return head == null && tail == null;
+// public static boolean isEmpty(){
+//   return head == null && tail == null;
+// }
+
+// // add function
+// public static void add(int data){
+//    Node newNode = new Node(data);
+//    if(head == null){
+//     head = tail = newNode;
+//     return;
+//    } 
+//    tail.next = newNode;
+//    tail = newNode;
+// }
+
+// // remove 
+// public static int remove(){
+//     if(isEmpty()){
+//         System.out.println("empty queue");
+//         return -1;
+//     }
+//        int front = head.data;
+//        if(tail == head){
+// tail = head = null;
+//        }else{
+//         head = head.next;
+//        }
+//        return front;
+// }
+
+// // peek 
+// public static int peek(){
+//     if(isEmpty()){
+//         System.out.println("empty queue");
+//         return -1;
+//     }
+
+//     return head.data;
+// }
+//     }
+
+static class QueueB{
+    static Stack<Integer> s1 = new Stack<>();
+    static Stack<Integer> s2 = new Stack<>();
+
+    public static boolean is
 }
-
-// add function
-public static void add(int data){
-   Node newNode = new Node(data);
-   if(head == null){
-    head = tail = newNode;
-    return;
-   } 
-   tail.next = newNode;
-   tail = newNode;
-}
-
-// remove 
-public static int remove(){
-    if(isEmpty()){
-        System.out.println("empty queue");
-        return -1;
-    }
-       int front = head.data;
-       if(tail == head){
-tail = head = null;
-       }else{
-        head = head.next;
-       }
-       return front;
-}
-
-// peek 
-public static int peek(){
-    if(isEmpty()){
-        System.out.println("empty queue");
-        return -1;
-    }
-
-    return head.data;
-}
-    }
     public static void main(String[] args) {
-        QueueB q = new QueueB();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        System.out.println(q.remove());
-        q.add(4);
-        System.out.println(q.remove());
-        q.add(5);
-        while (!q.isEmpty()) {
-            System.out.println(q.peek());
-            q.remove();
-        }
+        // QueueB q = new QueueB();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // System.out.println(q.remove());
+        // q.add(4);
+        // System.out.println(q.remove());
+        // q.add(5);
+        // while (!q.isEmpty()) {
+        //     System.out.println(q.peek());
+        //     q.remove();
+        // }
+
+
     }
 }
