ccd81067d8d3262f29398185a20d2da42898479e
aashirwad89 <studyonly806@gmail.com>
2026-03-25 13:00:23 +0530

    queue ques solved

--- Queue.java
+++ Queue.java
@@ -1,7 +1,6 @@
-import java.util.Stack;
-import java.utils.*;
+import java.util.*;
 
-public class Queue {
+public class QueueB {
 //     static class Node{
 //         int data;
 //          Node next;
@@ -58,13 +57,111 @@ public class Queue {
 // }
 //     }
 
-static class QueueB{
-    static Stack<Integer> s1 = new Stack<>();
-    static Stack<Integer> s2 = new Stack<>();
+// static class QueueB{
+//     static Stack<Integer> s1 = new Stack<>();
+//     static Stack<Integer> s2 = new Stack<>();
 
-    public static boolean is
+//     public static boolean isEmpty(){
+//         return s1.isEmpty();
+//     }
+//     // add
+//     public static void add(int data){
+//         while(!s1.isEmpty()){
+//             s2.push(s1.pop());
+//         }
+//         s1.push(data);
+
+//         while(!s2.isEmpty()){
+//             s1.push(s2.pop());
+//         }
+//     }
+
+//     // remove
+//     public static int remove(){
+//         if(isEmpty()){
+//             System.out.println("queue is empty");
+//             return -1;
+//         }
+
+//         return s1.pop();
+//     }
+
+//     // peek
+//     public static int peek(){
+//         if(isEmpty()){
+//             System.out.println("queue is empty");
+//             return -1;
+//         }
+
+//         return s1.peek();
+
+//             }
+// }
+  static class Stack{
+static Queue<Integer> q1 = new LinkedList<>();
+    static Queue<Integer> q2 = new LinkedList<>();
+
+    public static boolean isEmpty() {
+        return q1.isEmpty() && q2.isEmpty();
+    }
+
+    public static void push(int data) {
+        if (!q1.isEmpty()) {
+            q1.add(data);
+        } else {
+            q2.add(data);
+        }
+    }
+
+    public static int pop() {
+        if (isEmpty()) {
+            System.out.println("empty stack");
+            return -1;
+        }
+
+        int top = -1;
+
+        if (!q1.isEmpty()) {
+            while (q1.size() > 1) {
+                q2.add(q1.remove());
+            }
+            top = q1.remove(); // last element
+        } else {
+            while (q2.size() > 1) {
+                q1.add(q2.remove());
+            }
+            top = q2.remove();
+        }
+
+        return top;
+    }
+
+    public static int peek() {
+        if (isEmpty()) {
+            System.out.println("empty stack");
+            return -1;
+        }
+
+        int top = -1;
+
+        if (!q1.isEmpty()) {
+            while (q1.size() > 1) {
+                q2.add(q1.remove());
+            }
+            top = q1.remove();
+            q2.add(top); // put it back
+        } else {
+            while (q2.size() > 1) {
+                q1.add(q2.remove());
+            }
+            top = q2.remove();
+            q1.add(top);
+        }
+
+        return top;
+    }
 }
-    public static void main(String[] args) {
+public static void main(String[] args) {
         // QueueB q = new QueueB();
         // q.add(1);
         // q.add(2);
@@ -78,6 +175,25 @@ static class QueueB{
         //     q.remove();
         // }
 
+// QueueB q = new QueueB();
+// q.add(1);
+// q.add(2);
+// q.add(3);
 
+// while(!q.isEmpty()){
+//     System.out.println(q.peek());
+//     q.remove();
+// }
+
+
+Stack s = new Stack();
+s.push(1);
+s.push(2);
+s.push(3);
+
+while(!s.isEmpty()){
+    System.out.println(s.peek());
+    s.pop();
+}
     }
 }
