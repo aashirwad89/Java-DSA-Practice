443fdb4b806442709ad1aa0134c8c15915dc56a8
aashirwad89 <studyonly806@gmail.com>
2026-05-08 15:54:18 +0530

    ontrack again

--- Greedy.java
+++ Greedy.java
@@ -1,27 +1,56 @@
 import java.util.*;
 public class Greedy {
     public static void main(String[] args) {
-        int start[] = {1,3,0,5,8,5};
-        int end[]= {2,4,6,7,9,9};
+//         int start[] = {1,3,0,5,8,5};
+//         int end[]= {2,4,6,7,9,9};
 
-        int maxAct = 0;
-        ArrayList<Integer> ans = new ArrayList<>();
+//         int maxAct = 0;
+//         ArrayList<Integer> ans = new ArrayList<>();
 
-        maxAct = 1;
-        ans.add(0);
-int lastEnd = end[0];
-        for(int i = 1; i<end.length; i++){
-            if(start[i] >= lastEnd){
-// activity select
-              maxAct++;
-              ans.add(i);
-              lastEnd = end[i];
-            }
-        }
+//         maxAct = 1;
+//         ans.add(0);
+// int lastEnd = end[0];
+//         for(int i = 1; i<end.length; i++){
+//             if(start[i] >= lastEnd){
+// // activity select
+//               maxAct++;
+//               ans.add(i);
+//               lastEnd = end[i];
+//             }
+//         }
 
-        System.out.println(maxAct);
-        for(int i = 0; i<ans.size(); i++){
-            System.out.println(ans.get(i));
-        }
+//         System.out.println(maxAct);
+//         for(int i = 0; i<ans.size(); i++){
+//             System.out.println(ans.get(i));
+//         }
+
+int val[] = {60, 100 , 120};
+int weight[] = {10 , 20 , 30};
+int W = 50;
+
+double ratio[][] = new double[val.length][2];
+
+
+for(int i = 0; i< val.length; i++){
+    ratio[i][0] = i;
+    ratio[i][1] = val[i]/(double)weight[i];
+}
+
+Arrays.sort(ratio , Comparator.comparingDouble(o -> o[1]));
+
+int capacity = W;
+int finalVal = 0;
+for(int i = ratio.length-1; i>=0; i--){
+    int idx = (int)ratio[i][0];
+ if(capacity >= weight[idx]){
+finalVal += val[idx];
+capacity -= weight[idx];
+ }else{
+    finalVal += (ratio[i][1] * capacity);
+    capacity = 0;
+    break;
+ }
+}
+System.out.println(finalVal);
     }
 }
