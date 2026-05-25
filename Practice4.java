7816c5df367c99d879d11a1d2dce67979e137c9e
aashirwad89 <studyonly806@gmail.com>
2026-05-25 16:02:22 +0530

    strings revision done

--- Practice4.java
+++ Practice4.java
@@ -5,30 +5,61 @@ public class Practice4 {
     //Arrays ---
 
     // Find largest element 
-    int arr[] = {5,6,7,8,32};
-    int max = arr[0];
+    // int arr[] = {5,6,7,8,32};
+    // int max = arr[0];
 
-    for(int i = 0; i<arr.length; i++){
-        if(arr[i]>max){
-            max = arr[i];
-        }
+    // for(int i = 0; i<arr.length; i++){
+    //     if(arr[i]>max){
+    //         max = arr[i];
+    //     }
+    // }
+    // System.out.println(max);
+
+    // //kadanes algo 
+    // int arr2[] = {-2,-3,-1,4,3,2,1};
+
+    // int maxSum = arr[0];
+    // int currSum = 0;
+
+    // for(int i = 0; i<arr2.length; i++){
+    //     currSum += i;
+    //     maxSum = Math.max(maxSum, currSum);
+
+    //     if(currSum <0){
+    //         currSum = 0;
+    //     }
+    // }
+    // System.out.println(maxSum);
+
+
+    //Strings ----
+
+    //reverse a string 
+    String str = "hello";
+    String rev = "";
+
+    for(int i = str.length()-1; i>=0; i--){
+        rev += str.charAt(i);
     }
-    System.out.println(max);
+    System.out.println(rev);
 
-    //kadanes algo 
-    int arr2[] = {-2,-3,-1,4,3,2,1};
 
-    int maxSum = arr[0];
-    int currSum = 0;
+    //check palindrome strings - two pointers 
+    String str2 = "radar";
 
-    for(int i = 0; i<arr2.length; i++){
-        currSum += i;
-        maxSum = Math.max(maxSum, currSum);
+    int start = 0;
+    int end = str2.length()-1;
 
-        if(currSum <0){
-            currSum = 0;
+    boolean isPalindrome = true;
+
+    while(start<end){
+        if(str2.charAt(start)!=str2.charAt(end)){
+            isPalindrome  = false;
+            break;
         }
+        start++;
+        end--;
     }
-    System.out.println(maxSum);
+    System.out.println(isPalindrome);
    }
 }
