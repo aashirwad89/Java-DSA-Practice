616c0bc94e6c8b4a734ba55647d345b34b10791b
aashirwad89 <studyonly806@gmail.com>
2026-05-28 10:05:04 +0530

    code

--- Revision2.java
+++ Revision2.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class Revision2 {
     public static void main(String[] args) {
-        
+        //Stack ----------
+
+        //valid parentheses 
+
+        X
     }
 }
