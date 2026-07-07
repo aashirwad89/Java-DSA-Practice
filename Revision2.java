9b1434c70bcac8458f11714b04418afd3108fa4d
aashirwad89 <studyonly806@gmail.com>
2026-07-07 06:15:11 +0530

    push

--- Revision2.java
+++ Revision2.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 public class Revision2 {
     public static void main(String[] args) {
-        //Stack ----------
-
-        //valid parentheses 
+       
         
 
         X
